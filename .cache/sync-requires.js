1577c859730fb483ad5fbef1adc2065764df9090
MatthewShields <info@matthewshields.co.uk>
2020-04-12 21:04:50 +0100

    Add secondary build cache

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---node-modules-gatsby-source-prismic-graphql-components-preview-page-js": hot(preferDefault(require("/Users/Matthew.Shields/prismictest/basicbuildtest/node_modules/gatsby-source-prismic-graphql/components/PreviewPage.js"))),
   "component---src-templates-post-js": hot(preferDefault(require("/Users/Matthew.Shields/prismictest/basicbuildtest/src/templates/post.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Matthew.Shields/prismictest/basicbuildtest/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/Matthew.Shields/prismictest/basicbuildtest/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/Matthew.Shields/prismictest/basicbuildtest/src/pages/index.js")))
 }
